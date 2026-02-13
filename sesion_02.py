e2b9e5e2727acd3c982d0f8973186c1df0a20a03
citlalimartinezgarcia <citlalimartinezgarcia95@gmail.com>
2026-02-12 18:40:52 -0800

    Descripcion del cambio que hiciste en el archivo sesion_02.py

---
+++ sesion_02.py
@@ -0,0 +1,9 @@
+#Tuplas
+mi_tupla =(2,4)
+print("mi tupla: ", mi_tupla)
+
+#lista
+mi_lista = [1,3.1416, "Citlali ",mi_tupla]
+print("el primer elemento de mi lista: ",mi_lista[0])
+print("el cuarto elemento de mi lista: ",mi_lista[3])
+print("el tercer elemento de mi lista> ",mi_lista[2])
