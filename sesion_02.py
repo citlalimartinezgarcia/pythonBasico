9925e7d852c13abc755a0ec5510785c51f22afef
citlalimartinezgarcia <citlalimartinezgarcia95@gmail.com>
2026-02-12 18:54:11 -0800

    Descripcion del cambio que hiciste en el archivo sesion_02.py

--- sesion_02.py
+++ sesion_02.py
@@ -7,3 +7,14 @@ mi_lista = [1,3.1416, "Citlali ",mi_tupla]
 print("el primer elemento de mi lista: ",mi_lista[0])
 print("el cuarto elemento de mi lista: ",mi_lista[3])
 print("el tercer elemento de mi lista> ",mi_lista[2])
+
+#Diccionarios
+mi_diccionario = {
+    "mi_lista ":mi_lista,
+    "nombre": "Citlali",
+    "pi": 3.1416,
+    "tel": "664-241-2901"
+}
+print("llave para accesar a mi diccionario mi_lista", mi_diccionario["mi_lista"])
+print("llave para accesar a mi diccionario pi ",mi_diccionario["pi"])
+print("llave para accesar a mi diccionario tel ",mi_diccionario["tel"])
